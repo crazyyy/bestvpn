c1465b8b338c101e7df3131b59ef0202f70c5e3d
Vitalii Antoniuk <va@jelasric.com>
2018-12-16 12:14:46 +0200

    close and archive project

--- wordpress/wp-content/themes/wp-vpn/page-comparsion.php
+++ wordpress/wp-content/themes/wp-vpn/page-comparsion.php
@@ -9,6 +9,40 @@
           <?php the_content(); ?>
 
 
+911
+
+
+</head>
+<body>
+
+<div id="images"></div>
+
+<script>
+(function() {
+
+
+
+$.getJSON("http://bestvpn.dev/wp-json/posts?type=servers", function (obj) {
+
+
+    $.each(obj.title, function (key, value) {
+     console.log( value.name);
+
+    });
+
+});
+
+})();
+</script>
+
+
+
+
+112
+
+
+
+
 <form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>/comparison-second-page">
   <label>
     <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
